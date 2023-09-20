c305248af7845816726ffe9b1a2050d12958441e
2hakjoon <2hakjoon@google.com>
2023-09-20 22:53:46 +0900

    주석 해제

--- app/login/processing/page.tsx
+++ app/login/processing/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { axiosClientQuery } from "@/util/axios/axios-intances";
 import { useSearchParams } from "next/navigation";
 
 export default function LoginProcessing() {
   const param = useSearchParams();
-  // const res = axiosClientQuery.get("/auth/login/kakao", {
-  //   params: { code: param.get("code") },
-  // });
+  const res = axiosClientQuery.get("/auth/login/kakao", {
+    params: { code: param.get("code") },
+  });
+  console.log("res: ", res);
   return <></>;
 }
